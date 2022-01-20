dab4ebd81a2c9a18071892bb910e66d4f18c2be4
saar120 <saar120@gmail.com>
2022-01-20 16:27:36 +0200

    build

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRouter = require("../routes/users.routes");
+const userRouter = require("./routes/users.routes");
 require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
+const publicPath = path.join(__dirname, "client/build");
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static(publicPath));
 
 const connectionURI = process.env.MONGODB_CONNECTION_URI;
 
