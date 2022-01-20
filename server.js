873d4f94f3382bc8bc4de0d95cc621c043d9f95b
saar120 <saar120@gmail.com>
2022-01-20 16:40:15 +0200

    path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/users.routes");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
