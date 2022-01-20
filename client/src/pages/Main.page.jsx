3c66e01894b54c541b3f0f1793970d6345f4e9fe
saar120 <saar120@gmail.com>
2022-01-20 23:39:37 +0200

    get user

--- client/src/pages/Main.page.jsx
+++ client/src/pages/Main.page.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import axios from "axios";
+import usersAPI from "../api/api.config";
 import InputForm from "../components/InputForm";
 
-const apiUrl = process.env.REACT_APP_API_URL;
 export default function MainPage() {
   // const [user, setUser] = useState(null);
 
   const getUser = async (id) => {
-    console.log(apiUrl);
     if (!id) return;
-    const { data } = await axios.get(apiUrl + id);
-    console.log(data);
+    try {
+      const { data } = await usersAPI.get(id);
+      console.log(data);
+    } catch (error) {
+      console.error(error.response.data.error || "Error");
+    }
   };
 
   return (
