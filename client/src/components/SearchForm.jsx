c32ca68da0d01fe06d0b42091c59b9cbae0dcf56
saar120 <saar120@gmail.com>
2022-01-22 17:53:02 +0200

    added submit on enter

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 export default function SearchForm({ callback }) {
   const [searchTerm, setSearchTerm] = useState("");
 
-  const getHandler = () => {
+  const getHandler = (e) => {
+    if (e.type === "keydown" && e.code !== "Enter") return;
+    console.log(e);
     callback(searchTerm);
     setSearchTerm("");
   };
@@ -11,7 +13,7 @@ export default function SearchForm({ callback }) {
   return (
     <div>
       <div className="form">
-        <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} type="text" />
+        <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} type="text" onKeyDown={getHandler} />
         <button onClick={getHandler}>Get User</button>
       </div>
     </div>
