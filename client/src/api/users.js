9762002e6069f128de87b002a2b7f9d67d684a35
saar120 <saar120@gmail.com>
2022-01-22 15:49:21 +0200

    updte

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -5,6 +5,11 @@ const getUserById = async (id) => {
   return data;
 };
 
+const getAllUsers = async () => {
+  const { data } = await usersAPI.get("/");
+  return data;
+};
+
 const deleteUser = async (id) => {
   const { data } = await usersAPI.delete(id);
   return data;
---
+++ client/src/pages/pages.styled/Page.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const PageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin: 1.5rem;
+`;
+
+export default PageStyled;
